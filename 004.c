55c0563beacf11c2be51269b8b53b677bd77fb16
sooyyoung <sooyyyoung@gmail.com>
2022-09-19 10:20:29 +0900

    구구단

---
+++ 004.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// 구구단
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+    for(int i = 0; i < 9; i++){
+        printf("%d * %d = %d\n", n, i + 1, n * (i + 1));
+    }
+	
+	return 0;
+}
