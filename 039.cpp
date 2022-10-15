1bd4d2cb46daa2699252e4f8060b450cd88de856
sooyoung <sooyyyoung@gmail.com>
2022-10-15 17:09:12 +0900

    아스키 코드

---
+++ 039.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// 아스키 코드
+
+// 입력: 알파벳 소문자, 대문자, 숫자 0-9 중 하나
+// 출력: 입력으로 주어진 글자의 아스키 코드 값
+// ex) A - 65, 0 - 48, z - 122
+
+int main() {
+	char c;
+	int a;
+
+	cin >> c;
+	a = c;
+	cout << a;
+}
